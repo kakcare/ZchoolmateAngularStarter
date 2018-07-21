e899ea481d180fc5732afaab5bd8dc78028a0e0b
napisoot <napisoot@foobarsystem.com>
2018-07-21 13:52:16 +0700

    playing with html event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-// test
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  debugx(event:KeyboardEvent){
+    console.log(event.key)
+  }
 }
